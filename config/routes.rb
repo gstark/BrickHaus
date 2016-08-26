2499755a038977124df12ad7f8dc69406e33320a
Alan Kebert <adkebert@yahoo.com>
2016-08-26 13:22:36 -0400

    added a homepage new page purely for testing authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :homes
+  devise_for :user
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/homepage/index'
+  get '/homepage/new'
 
   root 'homepage#index'
 end
