17245c5620f21b56ac60e6c68b758eea6aced0ad
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-27 17:16:58 -0400

    issues

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,10 +14,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    params.require(:user).permit(:name, :phone, :address, :is_agent, :email, :password, :password_confirmation)
+    params.require(:user).permit(:profile_image, :name, :phone, :address, :is_agent, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :phone, :address, :is_agent, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:profile_image, :name, :phone, :address, :is_agent, :email, :password, :password_confirmation, :current_password)
   end
 end
