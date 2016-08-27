3151f1360a87eee8a42ecd10c4686d2a55ff0711
Alan Kebert <adkebert@yahoo.com>
2016-08-27 09:54:44 -0400

    demonstrated the router

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'user/:id' => 'registrations#show'
     get 'users'    => 'registrations#index'
+    get 'registrations/whiteboard' => 'registrations#whiteboard'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
