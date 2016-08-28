eae22e1647f72072c3860204c843252ffaaf5f64
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-28 17:09:51 -0400

    seed file issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ home = Home.create!(address: "444 Jon Colegrove Street, Pellston, MI 49769",
                     price: 120_000,
                     description: "When Jon Colegrove, a local restoration expert, transplanted this home to its current location on St. Julian Street, workers reported odd noises and a tall man dressed in black glaring through a bedroom window. Since, stories of ghostly visions and more have surrounded the late 18th-century house. Some even claim it’s Savannah’s most haunted home.",
                     main_image: File.open(Rails.root.join("db/seeds/house5.jpg")),
-                    additional_image: File.open(Rails.root.join("db/seeds/house25.jpg"))
+                    additional_image: File.open(Rails.root.join("db/seeds/house27.jpg"))
                     )
 
 home = Home.create!(address: "424242 Gavin Stark Way, Ruby, FL, 33701",
@@ -55,4 +55,44 @@ home = Home.create!(address: "424242 Gavin Stark Way, Ruby, FL, 33701",
                     description: "The luxury continues inside, with a 10-car garage perfect for the avid Tesla enthusiast, a wine cellar with storage for up to 500 bottles for the esteemed collector, and plenty of entertainment options, including a 12-seat home theater, billiards room, and gym with sauna.",
                     main_image: File.open(Rails.root.join("db/seeds/house6.jpg")),
                     additional_image: File.open(Rails.root.join("db/seeds/house26.jpg"))
+                    )
+
+home = Home.create!(address: "10 Frontenders Road, Javascript, FL, 33702",
+                    beds: 4,
+                    baths: 2,
+                    square_footage: 21_000,
+                    price: 210_000,
+                    description: "Have you ever thought,”Is today going to change my life?” Well even though this home may not change your life, its going to give you a great place to live! It has vinyl siding, adorable curb appeal and features mother in law suite upstairs. Built in 1910, [the city] was but a wee baby when this home was built. The stories it must have, huh? The wood floors are great, and floor plan had nice flow. You may not have a horse and carriage, but you will appreciate its history I am sure.",
+                    main_image: File.open(Rails.root.join("db/seeds/house7.jpg")),
+                    additional_image: File.open(Rails.root.join("db/seeds/house25.jpg"))
+                    )
+
+home = Home.create!(address: "10 Frontenders Road, Javascript, FL, 33702",
+                    beds: 4,
+                    baths: 2,
+                    square_footage: 21_000,
+                    price: 210_000,
+                    description: "So let me start by being honest, I lost my kitchen. BUT, my glass is half full and this means no tear out when you were going to update me anyways! I do have wood floors, open feel and rural like lot. My plumbing is shot and is AS-IS. I do have detached one car garage though, full bath on main, and am located in [desirable, top-ranked school district] -so overlook the few short falls and bring your work gloves, I have potential",
+                    main_image: File.open(Rails.root.join("db/seeds/house8.jpg")),
+                    additional_image: File.open(Rails.root.join("db/seeds/house28.jpg"))
+                    )
+
+home = Home.create!(address: "30 MiddleEnders Street, .Net, FL, 33703",
+                    beds: 6,
+                    baths: 3,
+                    square_footage: 33_000,
+                    price: 330_000,
+                    description: "I think its best to start here to save on gas due to it costing so much. But if you feel like you need to drive around for no reason just to make sure, that is fine. Its not my time you are wasting. I have new paint, carpet and appliances. Hot water boiler is nice and large deck off the rear. Lot could probably hold a garage if you need it, but make sure! Shed for additional storage. Look forward to meeting you. -Signed -ADORABLE HOME.",
+                    main_image: File.open(Rails.root.join("db/seeds/house9.jpg")),
+                    additional_image: File.open(Rails.root.join("db/seeds/house29.jpg"))
+                    )
+
+home = Home.create!(address: "777 NoEnd to Enders Circle, Programming, FL, 33704",
+                    beds: 7,
+                    baths: 4,
+                    square_footage: 4_000,
+                    price: 430_000,
+                    description: "The home also has a giant basement for storage or finishing, so feel free to tell your mom the home has a basement 'in case of storms' and she will almost certainly nod in approval. You're welcome, mom.",
+                    main_image: File.open(Rails.root.join("db/seeds/house10.jpg")),
+                    additional_image: File.open(Rails.root.join("db/seeds/house30.jpg"))
                     )
