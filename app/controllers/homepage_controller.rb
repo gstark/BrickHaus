bdf9fec31117cd1ad07449bed3fe8a47d2ed9fc4
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-29 18:15:28 -0400

    Additional revisions as per guidance from Gavin

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,11 +2,6 @@ class HomepageController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    homes = Home.all
-    unless Home.all.empty?
-      @recent_homes = homes.last(3)
-    else
-      "Great listings coming soon!"
-    end
+    @recent_homes = Home.last(3)
   end
 end
