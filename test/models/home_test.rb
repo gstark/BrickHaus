99790dc0beb2d9c92d64eefd49395c43d41bdb0b
Jonathan Colegrove <Demetrius472@gmail.com>
2016-08-31 11:45:27 -0400

    fixed carousel slider

--- test/models/home_test.rb
+++ test/models/home_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
+require "minitest/autorun"
 
 class HomeTest < ActiveSupport::TestCase
+  # class HomeTest < Minitest::Test
   # test "the truth" do
   #   assert true
   # end
+
+  def test_checking_if_agent_authorized
+    home = Home.new
+    @user = home.user_authorized?(agent)
+    assert_equal home.user_authorized?(agent), @user
+  end
+
 end
